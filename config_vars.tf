5e75a68104f609d4247125df1bafad53825396ad
DoubleNNs <nnamdianyanwu@gmail.com>
2018-02-19 02:36:36 +0000

    API keys to access AWS account

---
+++ config_vars.tf
@@ -0,0 +1,11 @@
+##########################
+# AWS Provider Variables #
+##########################
+
+# Can hardcode access/secret keys or can leave it black to have Terraform
+# prompt for the values upon plan/apply
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
